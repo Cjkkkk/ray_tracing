2ef2ef764a10ab041717194e162ae69d1dbad5d7
cjkkkk <656569648@qq.com>
2018-11-30 01:55:15 +0800

    add vec3 class

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ray_tracing
 # chapter 1
+```
 PPM format
 P3
 [column] [row]
@@ -9,7 +10,7 @@ P3
 ...
 ...
 [R][G][B]
-
+```
 
 tool:
 [PPM预览器](http://paulcuth.me.uk/netpbm-viewer/)
--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-
+#include "vec3.h"
 using namespace std;
 int main() {
-    int nx = 200;
-    int ny = 100;
+    int nx = 200; // 200 column
+    int ny = 100; // 100 row
     cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1 ; j >= 0 ; j--){
         for(int i = 0; i < nx ; i++) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
             cout << ir << " " << ig << " " << ib << "\n";
         }
     }
