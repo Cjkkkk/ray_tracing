54776ca01a3ed4e3c01fd42515345ec2f823010a
cjkkkk <656569648@qq.com>
2019-02-26 23:57:57 +0800

    Update image texture

---
+++ moving_sphere.h
@@ -0,0 +1,8 @@
+//
+// Created by DELL on 2019/2/26.
+//
+
+#ifndef RAY_TRACING_MOVING_SPHERE_H
+#define RAY_TRACING_MOVING_SPHERE_H
+
+#endif //RAY_TRACING_MOVING_SPHERE_H
---
+++ stb_image.h
@@ -0,0 +1,8 @@
+//
+// Created by DELL on 2019/2/26.
+//
+
+#ifndef RAY_TRACING_STB_IMAGE_H
+#define RAY_TRACING_STB_IMAGE_H
+
+#endif //RAY_TRACING_STB_IMAGE_H
